e800f6c1b8f8a5fecc1308d250406c8bae902c73
Matt Snow <matthew.j.snow@gmail.com>
2017-09-30 19:52:08 -0600

    PART 2 DONE

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import { Header, Card, Container } from 'semantic-ui-react';
+import { Header, Card, Container, Button } from 'semantic-ui-react';
 import axios from 'axios';
 
 class Beers extends React.Component {
-  state = { totalPages: 0, beers: [] }
+  state = { page: 1, totalPages: 0, beers: [] }
 
   componentDidMount() {
-  axios.get('api/all_beers')
+  axios.get('api/all_beers?page=1&per_page=10')
     .then( res => {
-      this.setState({ totalPages: res.data.total_pages, beers: res.data.entries })
+      this.setState({ page: res.data.page, totalPages: res.data.total_pages, beers: res.data.entries })
     })
 }
+
+  nextPage = () => {
+    const page = this.state.page + 1
+    axios.get(`/api/all_beers?page=${page}&per_page=10`)
+      .then( res => {
+        this.setState({ page: res.data.page, beers: res.data.entries })
+      })
+  }
+
+  prevPage = () => {
+    const page = this.state.page - 1
+    axios.get(`/api/all_beers?page=${page}&per_page=10`)
+      .then( res => {
+        this.setState({ page: res.data.page, beers: res.data.entries })
+      })
+  }
+
   render() {
-    let {beers, totalPages} = this.state
+    let {beers, page, totalPages} = this.state
     return (
       <Container>
         <Header as='h1' color='green' textAlign='center'>
-          Beers
+          Beers page {page} of {totalPages}
+          <div>
+           <Button
+             content='Previous Page'
+             color='green'
+             inverted
+             icon='angle double left'
+             labelPosition='left'
+             onClick={this.prevPage}
+           />
+           <Button
+             content='Next Page'
+             color='green'
+             inverted
+             icon='angle double right'
+             labelPosition='right'
+             onClick={this.nextPage}
+           />
+          </div>
         </Header>
         <Card.Group itemsPerRow={1}>
           { beers.map( beer =>
--- client/src/components/Breweries.js
+++ client/src/components/Breweries.js
@@ -1,29 +1,65 @@
 import React from 'react';
-import { Header, Card, Container, Image, Feed } from 'semantic-ui-react';
+import { Header, Card, Container, Image, Button } from 'semantic-ui-react';
 import axios from 'axios';
 
 class Breweries extends React.Component {
-  state = { breweries: [] }
+  state = { page: 1, totalPages: 0, breweries: [] }
 
   componentDidMount() {
-  axios.get('api/all_breweries')
+  axios.get('api/all_breweries?page=1&per_page=9')
     .then( res => {
-      this.setState({ breweries: res.data.entries })
+      this.setState({ page: res.data.page, totalPages: res.data.total_pages, breweries: res.data.entries })
     })
-}
+  }
+
+  nextPage = () => {
+    const page = this.state.page + 1
+    axios.get(`/api/all_breweries?page=${page}&per_page=9`)
+      .then( res => {
+        this.setState({ page: res.data.page, breweries: res.data.entries })
+      })
+  }
+
+  prevPage = () => {
+    const page = this.state.page - 1
+    axios.get(`/api/all_breweries?page=${page}&per_page=9`)
+      .then( res => {
+        this.setState({ page: res.data.page, breweries: res.data.entries })
+      })
+  }
+
   render() {
+    let {breweries, page, totalPages} = this.state
     return (
       <Container>
         <Header as='h1' color='green' textAlign='center'>
-          Breweries
+          Breweries page {page} of {totalPages}
+          <div>
+           <Button
+             content='Previous Page'
+             color='green'
+             inverted
+             icon='angle double left'
+             labelPosition='left'
+             onClick={this.prevPage}
+           />
+           <Button
+             content='Next Page'
+             color='green'
+             inverted
+             icon='angle double right'
+             labelPosition='right'
+             onClick={this.nextPage}
+           />
+          </div>
         </Header>
         <Card.Group itemsPerRow={3}>
           { this.state.breweries.map( brewery => {
             let { id,
               name,
-              established,
-              description,
-              images = {"medium": "https://static1.squarespace.com/static/51b75a21e4b0d3389493d5b4/t/51ca6598e4b0898df4bf2b3f/1372218879896/beersensoryscience.jpg"}, 
+              established = "unknown",
+              description = "This is a phenomenal brewery. You're going to be very pleased with the beer we send out. It's the best.",
+              images = {"medium": "https://static1.squarespace.com/static/51b75a21e4b0d3389493d5b4/t/51ca6598e4b0898df4bf2b3f/1372218879896/beersensoryscience.jpg"},
               website
             } = brewery;
             return(
